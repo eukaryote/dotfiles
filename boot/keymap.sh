237fcd941b150dca534cb17d4100d6855ec6d247
eukaryote <sapientdust+github@gmail.com>
2014-04-26 08:04:04 -0700

    comment

--- boot/keymap.sh
+++ boot/keymap.sh
@@ -4,7 +4,8 @@
 # can also enjoy the pleasure of a useful CapsLock key.
 
 # This should be loaded when booting. On Ubuntu & Debian, you could
-# modify /etc/rc.local by adding a line to call this script.
+# modify /etc/rc.local by adding a line to call this script
+# (and making it executable if it isn't already).
 
 # Adapted from http://linuxcommando.blogspot.com/2008/03/remap-caps-lock-key-for-virtual-console.html
 
