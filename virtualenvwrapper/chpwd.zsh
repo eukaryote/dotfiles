8efbc1dea9310c98d5e72077cd33da3bd8e53678
eukaryote <sapientdust+github@gmail.com>
2014-04-28 20:22:11 -0700

    zsh helper to automatically activate a virtualenv upon entering if there is a .venv file in the directory containing the name of a virtualenv that isn't currently activated

---
+++ virtualenvwrapper/chpwd.zsh
@@ -0,0 +1,21 @@
+# Source this during zsh initialization, and add a '.venv' file to a
+# virtualenvwrapper project directory that contains the name of the
+# virtualenv or venv, and when you cd into the directory,
+# 'workon' will be called to activate the environment if it's not
+# already activated.
+
+# After each change of directory in zsh, check to see if there's a '.venv'
+# file, and if there is, then activate the virtualenv that is named inside
+# the file
+chpwd() {
+    if [[ ! -s .venv ]]
+    then
+        return 0
+    fi
+
+    local venv="$(cat .venv)"
+    if [[ "${venv}" !=  "${VIRTUAL_ENV##*/}" && -d "${WORKON_HOME}/${venv}" ]]
+    then
+        workon "${venv}"
+    fi
+}
